c911bd9701eb1c826166cf8b738315d6ac65a979
Noam Koren <noamko25@gmail.com>
2021-05-06 19:05:30 +0300

    moved external files to public

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Express = require("express");
 const app = Express();
-
+app.use(Express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
-app.listen(500, () =>  console.log("listening on port 5000"));
+app.listen(5000, () =>  console.log("listening on port 5000"));
