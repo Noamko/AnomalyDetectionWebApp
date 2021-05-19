ba8ac388eb7968ec5550bfbad8c2a63809e7ebad
Noam Koren <noamko25@gmail.com>
2021-05-20 00:16:51 +0300

    added csv upload form

--- public/js/drag_drop.js
+++ public/js/drag_drop.js
@@ -1,27 +1,7 @@
-const train_div = document.getElementById('train_div');
-const detect_div = document.getElementById('detect_div');
-const train_Tab = document.getElementById("trainTab");
-const detect_Tab = document.getElementById("detectTab");
-
-function trainTabClick() {
-	train_div.style.display = "block";
-	detect_div.style.display = "none";
-	train_Tab.className = "tabButton left active"
-	detect_Tab.className = "tabButton right";
+function handleFiles(files) {
+  ([...files]).forEach(uploadFile)
 }
 
-function detectTabClick() {
-	const train_div = document.getElementById('train_div');
-	const detect_div = document.getElementById('detect_div');
-	train_div.style.display = "none";
-	detect_div.style.display = "block";
-	train_Tab.className = "tabButton left"
-	detect_Tab.className = "tabButton right active";
+function uploadFile(file) {
+	
 }
-
-let dropArea = document.getElementById('drop-area');
-
-dropArea.addEventListener('dragenter', handlerFunction, false);
-dropArea.addEventListener('dragleave', handlerFunction, false);
-dropArea.addEventListener('dragover', handlerFunction, false);
-dropArea.addEventListener('drop', handlerFunction, false);
--- server.js
+++ server.js
@@ -1,16 +1,62 @@
 const Express = require("express");
+const fileUpload = require('express-fileupload');
 const app = Express();
+
 app.use(Express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-// POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
-})
+// default options
+app.use(fileUpload());
 
+app.post('/upload', function(req, res) {
+  let csv_train_file;
+  let csv_anomaly_file;
+  let uploadPath;
 
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
 
+  csv_train_file = req.files.csv_train_file;
+  csv_anomaly_file = req.files.csv_anomaly_file;
+  trainUploadPath = __dirname + '/tmp/' + "train.csv";
+  anomalyUploadPath = __dirname + '/tmp/' + "anomaly.csv";
+
+  // Use the mv() method to place the file somewhere on your server
+  csv_train_file.mv(trainUploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+  });
+   csv_anomaly_file.mv(anomalyUploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+  });
+   res.sendFile(__dirname +'/index.html');
+});
+
+app.post('/upload-anomaly', function(req, res) {
+  let csv_file;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  csv_file = req.files.csv_file;
+  uploadPath = __dirname + '/tmp/' + "anoamly.csv"
+
+  // Use the mv() method to place the file somewhere on your server
+  csv_file.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send("/");
+  });
+});
 
 app.listen(8080, () =>  console.log("listening on port 8080"));
