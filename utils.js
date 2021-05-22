3b2885c5ebb9a5d1ae7d5edee53b16cdb226c495
Ilan bogomolnik <naligob@gmail.com>
2021-05-22 21:06:44 +0300

    csv to map update
    with the export function we get a map of the data in the csv

---
+++ utils.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+/**
+ * In server.js write the following line. (in the top of the page)
+ * const csvParseToMap = require("./utils"); 
+ * 
+ * Then you can get a map by running csvToMap function :
+ * example. (To print the map!)
+ * console.log(csvParseToMap.csvToMap('tmp\\anomaly.csv'));
+
+ * @param {string} pathFile 
+ * @returns {Map}
+ */
+module.exports.csvToMap = function csvToMap(pathFile){
+    const data = fs.readFileSync(pathFile);
+    return parseCSVToMap(data.toString(),",");
+}
+function parseCSVToMap(str, delimiter = ",") {
+    var map = new Map()
+    const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+    console.log(headers.length);
+    var size = Object.keys(headers).length;
+    for (let i =0; i<size; i++) {
+        map.set(headers[i], [])
+    }
+    
+    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+    var temp = new Array(size);
+    for (var i =0; i<size;i++) {
+        temp[i] = [];
+    }    
+    for (var j=0;j<rows.length;j++) {
+        var row = rows[j].slice(0,rows[j].length).split(delimiter);
+        for (var i=0; i<temp.length;i++) {
+            temp[i].push(row[i]); 
+        }
+    }
+    for (let i =0; i<size; i++) {
+        map.set(headers[i], temp[i])
+    }
+    
+    return map;
+  }
